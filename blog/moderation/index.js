c98a6311769b20addbbaed616ed9567004cae40b
MiftahulxHuda <miftahulxhuda@gmail.com>
2020-08-02 15:40:02 +0700

    updating comment content

--- blog/moderation/index.js
+++ blog/moderation/index.js
@@ -5,10 +5,10 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   const { type, data } = req.body;
 
-  if(type === 'CommentCreated') {
+  if (type === 'CommentCreated') {
     const status = data.content.includes('orange') ? 'rejected' : 'approved';
 
     await axios.post('http://localhost:4005/events', {
@@ -21,6 +21,8 @@ app.post('/events', (req, res) => {
       }
     });
   }
+
+  res.send({});
 });
 
 app.listen(4003, () => {
